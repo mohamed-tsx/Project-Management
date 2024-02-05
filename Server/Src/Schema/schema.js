01713daf649332bb41bc4a19399e8af99b5ed08d
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-05 20:56:03 +0300

    Made some practice in Graphql with Sample data and connected to mongo db

---
+++ Server/Src/Schema/schema.js
@@ -0,0 +1,70 @@
+const { projects, clients } = require("../../SampleData");
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require("graphql");
+
+const clientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const projectType = new GraphQLObjectType({
+  name: "Project", // Corrected the type name to "Project"
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: clientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === parent.clientId);
+      },
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    projects: {
+      type: new GraphQLList(projectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: projectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+    clients: {
+      type: new GraphQLList(clientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
+    client: {
+      type: clientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
--- Server/index.js
+++ Server/index.js
@@ -2,16 +2,18 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./Src/Schema/schema");
+const colors = require("colors");
+const connectDB = require("./Config/db");
 const port = process.env.PORT || 3000;
 
 const app = express();
+connectDB();
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "development",
-    
   })
 );
 app.listen(port, console.log(`Server is running on port ${port}`));
